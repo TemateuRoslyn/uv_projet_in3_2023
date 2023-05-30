a09d468b36c0cb61c699334cc37262e9f3149812
Valery <nguezetpablovalery@gmail.com>
2023-05-30 20:08:57 +0100

    valer

---
+++ src/backend/laravel-app/app/Http/Controllers/PersonnelController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePersonnelRequest;
+use App\Http\Requests\UpdatePersonnelRequest;
+use App\Models\Personnel;
+
+class PersonnelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePersonnelRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Personnel $personnel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Personnel $personnel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePersonnelRequest $request, Personnel $personnel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Personnel $personnel)
+    {
+        //
+    }
+}
---
+++ src/backend/laravel-app/database/migrations/2023_05_30_185340_create_personnels_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personnels', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->date('date_nais');
+            $table->string('lieu_nais');
+            $table->string('photo');
+            $table->string('email');
+            $table->string('login');
+            $table->string('mot_de_passe');
+            $table->string('sexe');
+            $table->int('tel');
+            $table->string('role');
+            $table->string('fonction');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personnels');
+    }
+};
