c052df42b6c818b26c3169829875fb040cd0aef3
fsentinelle3 <120215291+fsentinelle3@users.noreply.github.com>
2023-05-31 19:13:07 +0300

    deleting old app

--- src/backend/laravel-app/database/factories/ParentaFactory.php
+++ src/backend/laravel-app/database/factories/ParentaFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Parents>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Parenta>
  */
-class ParentsFactory extends Factory
+class ParentaFactory extends Factory
 {
     /**
      * Define the model's default state.
--- src/backend/laravel-app/database/migrations/2023_05_30_195654_create_parentas_table.php
+++ src/backend/laravel-app/database/migrations/2023_05_30_195654_create_parentas_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('parents', function (Blueprint $table) {
+        Schema::create('parentas', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('nom');
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('parents');
+        Schema::dropIfExists('parentas');
     }
 };
