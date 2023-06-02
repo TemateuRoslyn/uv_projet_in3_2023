75d38b98cb25288c63ea6837168f806529b4f625
Ange Perside <angentinkam53@gmail.com>
2023-06-02 13:01:51 +0100

    Closes #56 - modification des icones

--- src/frontend/web/react-admin/src/pages/Convocations.tsx
+++ src/frontend/web/react-admin/src/pages/Convocations.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Breadcrumb from '../components/Breadcrumb';
 import DefaultLayout from '../layout/DefaultLayout'
 import ChatCard from '../components/ChatCard'
 
 const Convocations = () => {
   return (
     <DefaultLayout>
+      <Breadcrumb pageName="Convocations" />
       <ChatCard/>
     </DefaultLayout>
   )
