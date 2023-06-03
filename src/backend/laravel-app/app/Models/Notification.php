47983c1606be34c95825b78a2be3db94c31a7b95
Temateu Roslyn <temateuroslynf32@gmail.com>
2023-06-03 21:44:02 +0100

    Closes #77 - Documentation swagger pour les route eleve au auth

--- src/backend/laravel-app/app/Models/Notification.php
+++ src/backend/laravel-app/app/Models/Notification.php
@@ -8,14 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'libelle',
-        'view',
-        'name'
-    ];
-
-    public function users()
-    {
-        return $this->belongsToMany(Professeur::class, 'user_notifications');
-    }
+    protected $guarded = [];
 }
--- src/backend/laravel-app/database/migrations/2023_05_30_000004_create_personnels_table.php
+++ src/backend/laravel-app/database/migrations/2023_05_30_000004_create_personnels_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('userls', function (Blueprint $table) {
+        Schema::create('personnels', function (Blueprint $table) {
             $table->id();
             $table->string('fonction');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('userls');
+        Schema::dropIfExists('users');
     }
 };
