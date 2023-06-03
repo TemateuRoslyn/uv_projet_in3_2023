3325640223a8c58f67a440b6d03ae6aebd78224d
tchoumi313 <75470956+tchoumi313@users.noreply.github.com>
2023-06-03 05:07:51 +0100

    Closes #65 - Add admin, user, permission and roles menus

---
+++ src/frontend/web/react-admin/src/pages/AdminUser.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DefaultLayout from '../layout/DefaultLayout'
+import Breadcrumb from '../components/Breadcrumb'
+
+const AdminUser = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName='Users' />
+      
+    </DefaultLayout>
+  )
+}
+
+export default AdminUser
