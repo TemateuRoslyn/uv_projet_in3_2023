3287ed71b7724b86ed1de44ae7da3c2d48ff2fbe
Temateu Roslyn <temateuroslynf32@gmail.com>
2023-06-03 21:44:02 +0100

    Closes #58 - Creation du controller Eleve et des routes associees

--- src/backend/laravel-app/app/Models/Eleve.php
+++ src/backend/laravel-app/app/Models/Eleve.php
@@ -8,7 +8,13 @@ class Eleve extends User
     protected $fillable = [
         'solvable',
         'redoublant',
+        'user_id',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id');
+    }
 
     public function parents()
     {
@@ -18,5 +24,19 @@ class Eleve extends User
     public function classe()
     {
         return $this->belongsTo(Classe::class);
+    }
+
+    public function assignUserFields(User $user)
+    {
+        $this->email = $user->email;
+        $this->password = $user->password;
+        $this->name = $user->name;
+        $this->first_name = $user->first_name;
+        $this->last_name = $user->last_name;
+        $this->date_de_naissance = $user->date_de_naissance;
+        $this->lieu_de_naissance = $user->lieu_de_naissance;
+        $this->photo = $user->photo;
+        $this->sexe = $user->sexe;
+        $this->telephone = $user->telephone;
     }
 }
