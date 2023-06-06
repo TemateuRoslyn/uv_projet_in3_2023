4a489ae07cce55ad33aebfd0cc9a6c86052d46f3
fksyntia20 <fksyntia@gmail.com>
2023-06-06 19:15:55 +0200

    Closes #112 - modification de la carte

--- src/frontend/web/react-admin/src/App.tsx
+++ src/frontend/web/react-admin/src/App.tsx
@@ -14,6 +14,7 @@ import Parents from './pages/Parents';
 import Cours from './pages/Cours';
 import Enseignant from './pages/Enseignant';
 import Convocations from './pages/Convocations';
+
 import Settings from './pages/Settings';
 import Tables from './pages/Tables';
 import Alerts from './pages/UiElements/Alerts';
@@ -64,6 +65,7 @@ function App() {
       <Route path="/cours" element={<Cours />} />
       <Route path="/enseignant" element={<Enseignant />} />
       <Route path="/convocations" element={<Convocations />} />
+     
       <Route path="/forms/form-elements" element={<FormElements />} />
       <Route path="/forms/form-layout" element={<FormLayout />} />
       <Route path="/tables" element={<Tables />} />
--- src/frontend/web/react-site/src/components/contact/Contact.jsx
+++ src/frontend/web/react-site/src/components/contact/Contact.jsx
@@ -3,7 +3,11 @@ import Back from "../common/back/Back"
 import "./contact.css"
 
 const Contact = () => {
-  const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
+  const map = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3945089.628012213!2d12.929731998285743!3d5.594114711933558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x106cdd94bfebc8f1%3A0xdb295bdfec6cb26c!2sCameroon!5e0!3m2!1sen!2snp!4v1652612232960!5m2!1sen!2snp" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer"';
+
+  //const map = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3991688.143656266!2d17.683401905469745!3d8.121442401771064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104d3c7078dbe237%3A0xc26a9e424a9e2cc8!2sAfrica!5e0!3m2!1sen!2snp!4v1652611480455!5m2!1sen!2snp" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer"';
+
+  //const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
   return (
     <>
       <Back title='Contact us' />
