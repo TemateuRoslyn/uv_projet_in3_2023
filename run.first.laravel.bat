d32fc2814e2d20c05c7a69584ffcc496381a4149
Temateu Roslyn <temateuroslynf32@gmail.com>
2023-06-06 16:20:01 +0100

    Closes  #125 - Les roles sont desormais disponible au path: helpers/roles.php et les permissions helpers/permssions.php en tant que contantes pour etre lue dans toute l'application.

--- run.first.laravel.bat
+++ run.first.laravel.bat
@@ -11,12 +11,14 @@ for /f "skip=1" %%p in ('netstat -aon ^| findstr "%port%"') do (
 )
 if %running% == yes (
   composer install
+  composer dumpautoload
   php artisan l5-swagger:generate
   php artisan migrate:fresh --seed
   php artisan jwt:secret
   php artisan serve
 ) else (
   composer install
+  composer dumpautoload
   php artisan l5-swagger:generate
   pphp artisan migrate:fresh --seed
   php artisan jwt:secret
--- run.first.laravel.sh
+++ run.first.laravel.sh
@@ -8,12 +8,14 @@ if sudo netstat -tulpn | grep "$port" > /dev/null
 then
     sudo kill -9 `sudo lsof -t -i:8000`
     composer install
+    composer dumpautoload
     php artisan l5-swagger:generate
     php artisan migrate:fresh --seed
     php artisan jwt:secret
     php artisan serve
 else
     composer install
+    composer dumpautoload
     php artisan l5-swagger:generate
     php artisan migrate:fresh --seed
     php artisan jwt:secret
