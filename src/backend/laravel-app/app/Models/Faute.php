117dcd638b47595c57aae10216c0d30caaa15f4e
duvaldev203 <74819278+duvaldev203@users.noreply.github.com>
2023-06-26 21:30:48 +0100

    Closes #191 entite Faute

--- src/backend/laravel-app/app/Models/Faute.php
+++ src/backend/laravel-app/app/Models/Faute.php
@@ -4,13 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Eleve;
+
+/**
+ * @OA\Schema(
+ *     required={"libelle", "gravite","eleve_id","regle_id"},
+ *     @OA\Xml(name="Faute"),
+ *     @OA\Property(property="id", type="integer", readOnly=true, example="1"),
+ *     @OA\Property(property="libelle", type="string", example="escalade"),
+ *     @OA\Property(property="gravite", type="string", example="recidif"),
+ *   @OA\Property(property="eleve_id", type="integer", readOnly=true, example="1"),
+ *   @OA\Property(property="regle_id", type="integer", readOnly=true, example="1"),
+ *     @OA\Property(property="created_at", ref="#/components/schemas/BaseModel/properties/created_at"),
+ *     @OA\Property(property="updated_at", ref="#/components/schemas/BaseModel/properties/updated_at"),
+ *     @OA\Property(property="deleted_at", ref="#/components/schemas/BaseModel/properties/deleted_at")
+ * )
+ *
+ * @method static create(array $array)
+ * @method static findOrFail($id)
+ * @method static paginate(int $int)
+ *
+ * Class Regle
+ *
+ * @package App\Models
+ */
 
 class Faute extends Model
 {
     use HasFactory;
     protected $fillable = [
         'id',
-        'libelleFaute',
+        'libelle',
         'gravite',
         'eleve_id',
         'regle_id'
