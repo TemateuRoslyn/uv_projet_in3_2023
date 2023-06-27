89dfcf187ae831bc3b6633058afbb5030608ed62
Ange Perside <angentinkam53@gmail.com>
2023-06-27 15:08:15 +0100

    m

---
+++ src/frontend/web/react-site/src/components/convocation/Convocation.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Back from "../common/back/Back"
+import "./convocation.css"
+
+const Convocation = () => {
+   return (
+    <>
+      <Back title='Convocations' />
+      <section className='contacts padding'>
+        <div className='container shadow flexSB'>
+          
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Convocation
