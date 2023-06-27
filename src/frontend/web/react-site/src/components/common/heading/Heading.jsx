e442d25f723137a634f3c28bc7b9d17e0de9d827
Ange Perside <angentinkam53@gmail.com>
2023-06-27 15:08:15 +0100

    m

--- src/frontend/web/react-site/src/components/common/heading/Heading.jsx
+++ src/frontend/web/react-site/src/components/common/heading/Heading.jsx
@@ -4,8 +4,8 @@ const Heading = ({ subtitle, title }) => {
   return (
     <>
       <div id='heading'>
-        <h3>{subtitle} </h3>
         <h1>{title} </h1>
+        <h3>{subtitle} </h3>
       </div>
     </>
   )
